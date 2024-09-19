fa85a064ec283d99e0b145bd539d0e88dde3fa81
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-09-19 20:36:42 +0400

    styles

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -8,12 +8,27 @@ function Home() {
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-4 text-xl font-semibold md:text-3xl">
-        The best pizza.
-        <br />
-        <span className="text-red-500">
-          Straight out of the oven, straight to you.
+        The best
+        <span className="relative inline-block before:absolute before:-inset-0.5 before:block before:-skew-y-3 before:bg-red-400">
+          <span className="relative text-white"> Pizza </span>
         </span>
+        .
+        <br />
+        <span>Straight out of the oven, straight to you.</span>
       </h1>
+      <p className="mt-2 text-lg text-gray-600">
+        Order your favorite{" "}
+        <span className="relative inline-block before:absolute before:-inset-0.5 before:block before:-skew-y-3 before:bg-red-400">
+          <span className="relative text-white"> Pizza </span>
+        </span>
+        , enjoy a delicious meal, and have fun!
+      </p>
+
+      <img
+        className="mb-3 mt-3 rounded-md"
+        src="/img/companyIMG.jpg"
+        alt="The last Slice"
+      />
 
       {username === "" ? (
         <CreateUser />
